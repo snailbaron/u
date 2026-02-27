f0c17e184eb2cc6e939d2d4320adb35aaa1dd729
snailbaron <snailbaron@gmail.com>
2026-02-27 19:36:28 +0900

    Do not SendInput directly from the hook

--- main.cpp
+++ main.cpp
@@ -9,6 +9,24 @@
 #include <format>
 
 
+void sendCodePointInput(uint16_t codePoint)
+{
+    auto inputs = std::array<INPUT, 2>{ };
+    std::memset(inputs.data(), 0, sizeof(INPUT)* inputs.size());
+
+    inputs.at(0).type = INPUT_KEYBOARD;
+    inputs.at(0).ki.wScan = codePoint;
+    inputs.at(0).ki.dwFlags = KEYEVENTF_UNICODE;
+
+    inputs.at(1).type = INPUT_KEYBOARD;
+    inputs.at(1).ki.wScan = codePoint;
+    inputs.at(1).ki.dwFlags = KEYEVENTF_UNICODE | KEYEVENTF_KEYUP;
+
+    std::cerr << std::format("sending code point 0x{:x}\n", codePoint);
+    CHECK(SendInput(
+        (UINT)inputs.size(), inputs.data(), sizeof(INPUT)));
+}
+
 class Hooker {
 public:
     bool processEvent(bool pressing, const KBDLLHOOKSTRUCT& kbd)
@@ -41,29 +59,21 @@ public:
             auto digit = 10 + kbd.vkCode - 'A';
             _number = 16 * _number + digit;
         } else if (kbd.vkCode == VK_RETURN) {
+            uint16_t codePoint = 0;
+            std::cerr << std::format("number: 0x{:x}\n", _number);
             if (_number >= 0 && _number <= 0xffff) {
-                auto codePoint = static_cast<uint16_t>(_number);
-
-                std::cerr << std::format("will send {:x}\n", codePoint);
-
-                auto inputs = std::array<INPUT, 2>{};
-                std::memset(inputs.data(), 0, sizeof(INPUT) * inputs.size());
-
-                inputs.at(0).type = INPUT_KEYBOARD;
-                inputs.at(0).ki.wScan = codePoint;
-                inputs.at(0).ki.dwFlags = KEYEVENTF_UNICODE;
-
-                inputs.at(1).type = INPUT_KEYBOARD;
-                inputs.at(1).ki.wScan = codePoint;
-                inputs.at(1).ki.dwFlags = KEYEVENTF_UNICODE | KEYEVENTF_KEYUP;
-
-                CHECK(SendInput(
-                    (UINT)inputs.size(), inputs.data(), sizeof(INPUT)));
+                codePoint = static_cast<uint16_t>(_number);
             }
-
+            std::cerr << "deactivate\n";
             _active = false;
             _number = 0;
-            std::cerr << "deactivate\n";
+
+            std::cerr << std::format("code point: 0x{:x}\n", codePoint);
+            if (codePoint > 0) {
+                std::cerr << std::format("will send {:x}\n", codePoint);
+                CHECK(PostThreadMessage(
+                    GetCurrentThreadId(), WM_USER, codePoint, 0));
+            }
         }
         return true;
     }
@@ -113,6 +123,12 @@ int main() try {
         if (ret == -1) {
             throw WindowsError{GetLastError(), "GetMessage"};
         }
+
+        if (msg.message == WM_USER) {
+            auto codePoint = static_cast<uint16_t>(msg.wParam);
+            sendCodePointInput(codePoint);
+        }
+
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
